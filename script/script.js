0e57873936673095453d13986c3abd92b3f47804
TokAhmet <ahmet.ordulu@hotmail.se>
2018-03-07 23:10:54 +0100

    update

--- script/script.js
+++ script/script.js
@@ -2,44 +2,72 @@
 
 async function getData(){ //jshint ignore:line
 
-	let myWeatherResponse = await fetchData("http://api.openweathermap.org/data/2.5/weather?id=2673730&units=metric&APPID=501fd8025b1109e72c5fc70dd0da9d45");
-	let myForecastResponse = await fetchData("http://api.openweathermap.org/data/2.5/forecast?id=2673730&units=metric&APPID=501fd8025b1109e72c5fc70dd0da9d45");
-	let iconResponse = "http://openweathermap.org/img/w/";
 
-// console.log(myWeatherResponse);
-// console.log(myWeatherResponse2.list[0].dt_txt + " " + myWeatherResponse2.list[0].main.temp + " " + myWeatherResponse2.city.name);
-console.log(myForecastResponse);
-
-let result = document.getElementById("result");
-let myUl = document.getElementById("myUl");
-let myLi = document.createElement("li");
+let myWeatherResponse = await fetchData("http://api.openweathermap.org/data/2.5/weather?id=2673730&units=metric&APPID=501fd8025b1109e72c5fc70dd0da9d45");
+let myForecastResponse = await fetchData("http://api.openweathermap.org/data/2.5/forecast?id=2673730&units=metric&APPID=501fd8025b1109e72c5fc70dd0da9d45");
+let iconResponse = "http://openweathermap.org/img/w/";
 
 let weatherIcon = document.createElement("img");
 weatherIcon.setAttribute("src", iconResponse + myWeatherResponse.weather[0].icon + ".png");
 
-let currentWeather = document.createTextNode(myWeatherResponse.name + " " + myWeatherResponse.main.temp + 
-	" " + Date(myWeatherResponse.dt));
+document.getElementById("city").innerHTML = myWeatherResponse.name;
+document.getElementById("date").innerHTML = new Date(Date(myWeatherResponse.dt)).toLocaleString();
+document.getElementById("temp").innerHTML = Math.floor(myWeatherResponse.main.temp) + "&degC";
+// var currentIcon = document.getElementById("icon");
+// currentIcon.appendChild(weatherIcon);
 
-myLi.appendChild(currentWeather);
-myLi.appendChild(weatherIcon);
-myUl.appendChild(myLi);
-result.appendChild(myUl);
+var dateObj = new Date();
 
-// let filterForecast = myWeatherResponse2.list.filter(value => value);
-myForecastResponse.list.forEach( value =>  {
-
-	let forecastLi = document.createElement("li");
-	let forecastWeather = document.createTextNode(myForecastResponse.city.name + " " + value.main.temp + " " + value.dt_txt);
-	let newIcon = document.createElement("img");
-	newIcon.setAttribute("src", iconResponse + value.weather[0].icon + ".png");
-	forecastLi.appendChild(forecastWeather);
-	forecastLi.appendChild(newIcon);
-	myUl.appendChild(forecastLi);
-
-});
-
+var month = "0" + (dateObj.getMonth() + 1); 
+if (month >= 10) {
+	month = dateObj.getMonth() + 1;
+}
+var day = "0" + (dateObj.getDate() + 1);
+if (day >= 10) {
+	day = dateObj.getDate();
 }
 
+var year = dateObj.getFullYear();
+
+var todayDate = year + "-" + month + "-" + day;
+
+/*
+let filterMaxTemp = myForecastResponse.list.filter(value => value.dt_txt.includes(todayDate))
+.reduce((a, b) => a.main.temp < b.main.temp ? a : b);
+let filterMinTemp = myForecastResponse.list.filter(value => value.dt_txt.includes(todayDate))
+.reduce((a, b) => a.main.temp > b.main.temp ? a : b);
+*/
+
+let futureDays = document.getElementById("future");
+
+for(var i = 7; i < myForecastResponse.list.length; i+=8){
+
+	let forecastDiv = document.createElement("div");
+	forecastDiv.setAttribute("class", "day");
+
+	let dateh5 = document.createElement("h5");
+	let forecastWeatherDate = document.createTextNode(new Date(myForecastResponse.list[i].dt_txt).toDateString());
+	dateh5.appendChild(forecastWeatherDate);
+
+	let tempMinh6= document.createElement("h6");
+	let forecastWeatherMinTemp = document.createTextNode("Min-Temp: " + myForecastResponse.list[i + 1].main.temp_min);
+	tempMinh6.appendChild(forecastWeatherMinTemp);
+
+	let tempMaxh6 = document.createElement("h6");
+	let forecastWeatherMaxTemp = document.createTextNode("Max-Temp: " + myForecastResponse.list[i].main.temp_max);
+	tempMaxh6.appendChild(forecastWeatherMaxTemp);
+
+	let newIcon = document.createElement("img");
+	newIcon.setAttribute("src", iconResponse + myForecastResponse.list[i].weather[0].icon + ".png");
+	
+	forecastDiv.appendChild(dateh5);
+	forecastDiv.appendChild(tempMinh6);
+	forecastDiv.appendChild(tempMaxh6);
+	forecastDiv.appendChild(newIcon);
+	futureDays.appendChild(forecastDiv);
+}
+
+}
 async function fetchData(url){ // jshint ignore:line
 
 	let promise = await fetch(url);
